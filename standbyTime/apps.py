c3c0dfebb7425221d353076e32a34153da403f5e
自宅PC <sai11121209@gmail.com>
2020-06-27 12:34:21 +0900

    #05 新規App追加

---
+++ standbyTime/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class StandbytimeConfig(AppConfig):
+    name = 'standbyTime'
--- static/mysite.js
+++ static/mysite.js
@@ -1,3 +1,13 @@
 $(function(){
+    $('head').append(
+        '<style type="text/css">#container { display: none; } #fade, #loader { display: block; }</style>'
+    );
 
+    jQuery.event.add(window,"load",function() { // 全ての読み込み完了後に呼ばれる関数
+        var pageH = $("#container").height();
+
+        $("#fade").css("height", pageH).delay(900).fadeOut(800);
+        $("#loader").delay(600).fadeOut(300);
+        $("#container").css("display", "block");
+    });
 });
