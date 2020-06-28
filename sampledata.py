4792afe0e77b82b74001fe23ba5a010f75b8ae3e
自宅PC <sai11121209@gmail.com>
2020-06-28 21:11:16 +0900

    # 18訂正

--- sampledata.py
+++ sampledata.py
@@ -6,7 +6,7 @@ path = path.join(path.dirname(__file__), "sampledata20200218.csv")
 df = pd.read_csv(path)
 
 engine = create_engine(
-    "postgres://stiarglydckmrw:d726c958201e561102606b1dc3ce840dc7aa3b1c1eb04d7e283b59c965e17796@ec2-35-173-94-156.compute-1.amazonaws.com:5432/d22lnkqidjha62"
+    "postgres://dhabazyzjfedir:2e4c78147c903a92957d41ccd261a37f384dcfb7277845ca6f7e13982b089229@ec2-35-172-73-125.compute-1.amazonaws.com:5432/d8nkht2bie0dah"
 )
 df.DataFrame.to_sql(
     standbytime_standbytimedata, con=engine, if_exists="append", index=False
