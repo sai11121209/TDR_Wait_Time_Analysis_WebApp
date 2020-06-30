fd2584bb8491c9fdd01d6be8aaf04906358e0119
自宅PC <sai11121209@gmail.com>
2020-06-30 22:55:50 +0900

    # 28 Apscheduler完成

---
+++ test_clock.py
@@ -0,0 +1,46 @@
+from apscheduler.schedulers.blocking import BlockingScheduler
+import tasks
+import requests as rq
+from django.utils import timezone
+from django.utils.timezone import localtime  # 追加
+from datetime import datetime as dt
+import api
+
+sched = BlockingScheduler()
+
+
+@sched.scheduled_job("interval", minutes=1)
+def timed_job_TDL():
+    parkInfo = {}
+    parks_calendars = api.get_parks_calendars()
+    time = localtime(timezone.now())
+    for info in parks_calendars:
+        if info["date"] == time.strftime("%Y-%m-%d"):
+            parkInfo[info["parkType"]] = info
+    """
+    if (
+        parkInfo["TDL"]["openTime"]
+        <= time.strftime("%H:%M")
+        <= parkInfo["TDL"]["closeTime"]
+    ):"""
+    if True:
+        tasks.insertdata("TDL", parkInfo)
+        print(parkInfo)
+        print("This job is run every three minutes.1")
+    else:
+        print("This job is run every three minutes.TDL")
+    """
+    if (
+        parkInfo["TDS"]["openTime"]
+        <= time.strftime("%H:%M")
+        <= parkInfo["TDS"]["closeTime"]
+    ):"""
+    if True:
+        tasks.insertdata("TDS", parkInfo)
+        print(parkInfo)
+        print("This job is run every three minutes.1")
+    else:
+        print("This job is run every three minutes.TDS")
+
+
+sched.start()
