4993f055e127ea92e67940c3c7dc1c3d3d0c2cdf
自宅PC <sai11121209@gmail.com>
2020-07-01 23:40:46 +0900

    #34 初期動作含めた全体的な修正 マップ機能追加

--- static/main.js
+++ static/main.js
@@ -1,9 +1,9 @@
-$(function(){
+$(function () {
     $('head').append(
         '<style type="text/css">#container { display: none; } #fade, #loader { display: block; }</style>'
     );
 
-    jQuery.event.add(window,"load",function() { // 全ての読み込み完了後に呼ばれる関数
+    jQuery.event.add(window, "load", function () { // 全ての読み込み完了後に呼ばれる関数
         var pageH = $("#container").height();
 
         $("#fade").css("height", pageH).delay(900).fadeOut(800);
